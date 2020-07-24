345f22f0240891b2a74673e3a537029c2fa5c6b2
Bel Sahn <beldsahn@gmail.com>
2020-07-23 23:46:47 -0500

    Update gradle-wrapper.properties and TwoCities.java

--- src/main/java/org/example/numbers/TwoCities.java
+++ src/main/java/org/example/numbers/TwoCities.java
@@ -1,6 +1,7 @@
 package org.example.numbers;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Distance Between Two Cities - Calculates the distance between two cities
@@ -8,28 +9,82 @@ import java.util.*;
  * finding coordinates for the cities like latitude and longitude.
  */
 public final class TwoCities {
-    private static final Set<String> cities = new TreeSet<>(Set.of(
-            "Honolulu", "Seattle", "Los Angeles", "Phoenix", "New York City", "Miami",
-            "Mexico City", "Veracuz", "Toronto", "Sao Paolo", "Rio de Janeiro",
-            "Tokyo", "Shanghai", "Beijing", "Seoul", "Madrid", "London", "Paris", "Rome", "Athens",
-            "Havana", "San Juan", "Lima", "Port au Prince", "Bogota", "Kingston",
-            "Cairo", "Luxor", "Nairobi", "Dar es Salaam", "Johannesburg", "Freetown",
-            "Monrovia", "Addis Ababa", "Kigali", "Lagos", "Akkra"
-    ));
+    enum Cities {
+        ADDIS_ABABA("AA", "[A]ddis [A]baba"),
+        AKKRA("AK", "[A][k]kra"),
+        CAIRO("CR", "[C]ai[r]o"),
+        DAR_ES_SALAAM("DS", "[D]ar es [S]alaam"),
+        FREETOWN("FT", "[F]ree[t]own"),
+        KIGALI("KGL", "[K]i[g]a[l]i"),
+        KINGSTON("KG", "[K]in[g]ston"),
+        LAGOS("LG", "[L]a[g]os"),
+        MONROVIA("MNR", "[M]o[n][r]ovia"),
+        NAIROBI("NR", "[N]ai[r]obi"),
+        PORT_AU_PRINCE("PP", "[P]ort-au-[P]rince"),
+        SOWETO("SWT", "[S]o[w]e[t]o");
 
+        private final String abbreviation;
+        private final String abbreviatedSimpleName;
+
+        Cities(String abbreviation, String abbreviatedSimpleName) {
+            this.abbreviation = abbreviation;
+            this.abbreviatedSimpleName = abbreviatedSimpleName;
+        }
+
+        public String getSimpleName() {
+            String result = this.getAbbreviatedSimpleName();
+            result = result.replace("[", "");
+            result = result.replace("]", "");
+            return result.substring(0, 1).toUpperCase() + result.substring(1);
+        }
+
+        public String getAbbreviation() {
+            return abbreviation;
+        }
+
+        public String getAbbreviatedSimpleName() {
+            return abbreviatedSimpleName;
+        }
+
+        public static List<String> getCityNameKeys() {
+            return List.of(
+                    "Addis Ababa:Akkra", "Addis Ababa:Cairo", "Addis Ababa:Dar es Salaam", "Addis Ababa:Freetown", "Addis Ababa:Kigali", "Addis Ababa:Kingston",
+                    "Addis Ababa:Lagos", "Addis Ababa:Monrovia", "Addis Ababa:Nairobi", "Addis Ababa:Port-au-Prince", "Addis Ababa:Soweto",
+                    "Akkra:Cairo", "Akkra:Dar es Salaam", "Akkra:Freetown", "Akkra:Kigali", "Akkra:Kingston",
+                    "Akkra:Lagos", "Akkra:Monrovia", "Akkra:Nairobi", "Akkra:Port-au-Prince", "Akkra:Soweto",
+                    "Cairo:Dar es Salaam", "Cairo:Freetown", "Cairo:Kigali", "Cairo:Kingston",
+                    "Cairo:Lagos", "Cairo:Monrovia", "Cairo:Nairobi", "Cairo:Port-au-Prince", "Cairo:Soweto",
+                    "Dar es Salaam:Freetown", "Dar es Salaam:Kigali", "Dar es Salaam:Kingston", "Dar es Salaam:Lagos",
+                    "Dar es Salaam:Monrovia", "Dar es Salaam:Nairobi", "Dar es Salaam:Port-au-Prince", "Dar es Salaam:Soweto",
+                    "Freetown:Kigali", "Freetown:Kingston", "Freetown:Lagos", "Freetown:Monrovia", "Freetown:Nairobi", "Freetown:Port-au-Prince", "Freetown:Soweto",
+                    "Kigali:Kingston", "Kigali:Lagos", "Kigali:Monrovia", "Kigali:Nairobi", "Kigali:Port-au-Prince", "Kigali:Soweto",
+                    "Kingston:Lagos", "Kingston:Monrovia", "Kingston:Nairobi", "Kingston:Port-au-Prince", "Kingston:Soweto",
+                    "Lagos:Monrovia", "Lagos:Nairobi", "Lagos:Port-au-Prince", "Lagos:Soweto",
+                    "Monrovia:Nairobi", "Monrovia:Port-au-Prince", "Monrovia:Soweto",
+                    "Nairobi:Port-au-Prince", "Nairobi:Soweto",
+                    "Port-au-Prince:Soweto"
+            );
+        }
+    }
+
+    private static Set<String> cityNames = new TreeSet<>();
     private static Map<String, String> citiesMap = new TreeMap<>();
 
     //TODO: Add all City permutations to a TreeMap, delimited by a colon; {K=[CityA:CityB], V=[M:KM]}
     //TODO: Then to separate, create a stream to separate by the colon, and collect to a TreeSet.
-    //private static final String[] CITIY_KV_SPLITTER = map.keySet().split(":");
+    //private static final String[] CITY_KV_SPLITTER = citiesMap.keySet().toString().split(":");
 
-    private TwoCities(){}
+    private TwoCities() {
+    }
 
     public static void displayCalculation(Scanner console) {
-        var result = 0.0;
+        var resultA = 0.0;
+        var resultB = 0.0;
         try (console) {
             System.out.println("---------- Distance between 2 Cities ----------");
-            System.out.println("List of Cities: " + cities);
+            Stream.of(Cities.values()).forEach(city -> cityNames.add(city.getAbbreviatedSimpleName()));
+            System.out.println("Cities: " + cityNames);
+
             System.out.println("Choose City #1: ");
             String cityA = console.next();
             System.out.println();
@@ -42,26 +97,27 @@ public final class TwoCities {
             String distanceChoice = console.next();
             System.out.println();
 
-            switch(distanceChoice.toLowerCase()) {
+            switch (distanceChoice.toLowerCase()) {
                 case "mi":
-                    result = calculateMiles(console, cityA, cityB);
+                    resultA = calculateMiles(console, distanceChoice.toLowerCase(), cityA, cityB);
                     break;
                 case "km":
-                    result = calculateKilometers(console, cityA, cityB);
+                    resultB = calculateKilometers(console, distanceChoice.toLowerCase(), cityA, cityB);
                     break;
                 default:
                     throw new AssertionError(distanceChoice + " is an invalid entry!");
             }
-            System.out.println(String.format("The distance between %s and %s is %.2f%s(s)", cityA, cityB, result, distanceChoice));
+            System.out.println(String.format("The distance between %s and %s is %.2f%s(s)", cityA, cityB, 0.0, distanceChoice));
+            citiesMap.put((cityA + ":" + cityB), (resultA + ":" + resultB));
         } catch (Exception e) {
             System.err.println("An Exception occurred while displaying the calculation: " + e);
             throw e;
         }
     }
 
-    private static double calculateMiles(Scanner scanner, String cityA, String cityB) {
+    private static double calculateMiles(Scanner scanner, String distanceChoice, String cityA, String cityB) {
         try (scanner) {
-            System.out.println("");
+            System.out.println();
             return 0.0;
         } catch (Exception e) {
             System.err.println("An Exception occurred " + e);
@@ -69,8 +125,9 @@ public final class TwoCities {
         }
     }
 
-    private static double calculateKilometers(Scanner scanner, String cityA, String cityB) {
+    private static double calculateKilometers(Scanner scanner, String distanceChoice, String cityA, String cityB) {
         try (scanner) {
+            System.out.println();
             return 0.0;
         } catch (Exception e) {
             System.err.println("An Exception occurred " + e);
