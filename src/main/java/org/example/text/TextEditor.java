be90681f094e43edcbcfc5a60a079ea1fd7869bf
Bel Sahn <beldsahn@gmail.com>
2020-12-07 13:38:11 -0600

    Successful CRU for a text file in the text editor.

--- src/main/java/org/example/text/TextEditor.java
+++ src/main/java/org/example/text/TextEditor.java
@@ -8,6 +8,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
@@ -15,13 +16,16 @@ import javafx.scene.layout.RowConstraints;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.example.constants.AppConstants;
+import org.example.utils.FileUtils;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.List;
 
+import static org.example.utils.FileUtils.MISC_DIR;
+
 public class TextEditor extends Application {
-    private static final String MISC_DIR = "src/main/resources/misc";
-    private static MyMenuHandler handler = new MyMenuHandler();
+    private static final MyMenuHandler MENU_HANDLER = new MyMenuHandler();
     private static final List<FileChooser.ExtensionFilter> FILE_EXT_LIST = List.of(
             new FileChooser.ExtensionFilter("All", "*"),
             new FileChooser.ExtensionFilter("Text Files", "*.txt"),
@@ -32,7 +36,8 @@ public class TextEditor extends Application {
     );
 
     private GridPane rootPane;
-    private static TextArea textArea = new TextArea();
+
+    private static final TextArea TEXT_AREA = new TextArea();
 
     public static void main(String[] args) {
         launch(args);
@@ -50,7 +55,7 @@ public class TextEditor extends Application {
 
     private void initUI(Stage stage) {
         var menu = menuBar(stage);
-        rootPane = gridLayout(menu, textArea);
+        rootPane = gridLayout(menu, TEXT_AREA);
         var scene = new Scene(rootPane, 280, 300);
 
         stage.setTitle("Text Editor");
@@ -87,63 +92,84 @@ public class TextEditor extends Application {
         var menu = new MenuBar();
         menu.prefWidthProperty().bind(stage.widthProperty());
 
-        var fileMenuItem = new Menu("File");
-        menu.getMenus().add(fileMenuItem);
+        var fileMenu = new Menu("_File");
+        menu.getMenus().add(fileMenu);
 
-        //Creates a new file for text editing
-        //i.e., activates the text-field for text editing.
-        var newMenuItem = new Menu("New");
-        fileMenuItem.getItems().add(newMenuItem);
-
+        var newMenuItem = createNewTab(stage);
         var openMenuItem = openFile(stage);
-        fileMenuItem.getItems().add(openMenuItem);
-
         var saveMenuItem = saveFile(stage);
-        fileMenuItem.getItems().add(saveMenuItem);
+        var exitMenuItem = exitItem();
+        fileMenu.getItems().addAll(newMenuItem, openMenuItem, saveMenuItem, exitMenuItem);
 
-        fileMenuItem.getItems().add(new SeparatorMenuItem());
-
-        var exitMenuItem = new Menu("Exit");
-        exitMenuItem.setOnAction(event -> Platform.exit());
-        fileMenuItem.getItems().add(exitMenuItem);
         return menu;
     }
 
-    //TODO: Opens a textual file and populates the text box with its contents.
-    private Menu openFile(Stage stage) {
+    private MenuItem openFile(Stage stage) {
+        var menu = new MenuItem("Open");
+        menu.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
+
         var fileChooser = new FileChooser();
         fileChooser.setInitialDirectory(new File(MISC_DIR));
         fileChooser.getExtensionFilters().addAll(FILE_EXT_LIST);
 
-        var menu = new Menu("Open");
         menu.setOnAction(event -> {
             File selectedFile = fileChooser.showOpenDialog(stage);
-            System.out.println("Opened: " + selectedFile.getAbsolutePath());
-            readFileContent(selectedFile);
+
+            if (selectedFile != null) {
+                readFileContent(Path.of(selectedFile.getAbsolutePath()));
+            } else {
+                System.out.println("Open File Operation was cancelled.");
+            }
         });
         return menu;
     }
 
-    //TODO: Saves the contents of the textbox to a textual file
-    private Menu saveFile(Stage stage) {
+    //TODO: "New" should create a new tab/window in the pane, and not a new file.
+    private MenuItem createNewTab(Stage stage) {
+        var menu = new MenuItem("New");
+        menu.setAccelerator(KeyCombination.keyCombination("Ctrl+N"));
+        menu.setOnAction(event -> TEXT_AREA.clear());
+        return menu;
+    }
+
+    private MenuItem saveFile(Stage stage) {
+        var menu = new MenuItem("Save");
+        menu.setAccelerator(KeyCombination.keyCombination("Ctrl+S"));
+
         var fileChooser = new FileChooser();
         fileChooser.setInitialDirectory(new File(MISC_DIR));
         fileChooser.setInitialFileName("file.txt");
 
-        var menu = new Menu("Save");
         menu.setOnAction(event -> {
-            File savedFile = fileChooser.showSaveDialog(stage);
-
-            //saves the contents of the text field to a file
-            System.out.println("Saved: " + savedFile.getAbsolutePath());
-            handler.doShowMessageDialog(event);
+            File file = fileChooser.showSaveDialog(stage);
+            if (file != null) {
+                writeFileContent(file);
+                MENU_HANDLER.doShowSaveDialog(file.getName());
+            } else {
+                System.out.println("Save File Operation was cancelled.");
+            }
         });
         return menu;
     }
 
-    private void readFileContent(File input) {
-        textArea.setText(input.getName());
-        textArea.setWrapText(true);
+    private MenuItem exitItem() {
+        var menu = new MenuItem("Exit");
+        menu.setAccelerator(KeyCombination.keyCombination("Esc"));
+        menu.setOnAction(event -> Platform.exit());
+        return menu;
+    }
+
+    private void readFileContent(Path path) {
+        System.out.println("Opened: " + path.getFileName());
+        TEXT_AREA.setText(FileUtils.readFileFrom(path));
+        TEXT_AREA.setWrapText(true);
+    }
+
+    private void writeFileContent(File file) {
+        final String content = TEXT_AREA.getText();
+        TEXT_AREA.commitValue();
+        TEXT_AREA.cancelEdit();
+        FileUtils.lockAndSave(file, content);
     }
 
     private static class MyMenuHandler implements EventHandler<ActionEvent> {
@@ -161,6 +187,14 @@ public class TextEditor extends Application {
             alert.setTitle("Information Dialog");
             alert.setHeaderText("Menu Item Selection Information");
             alert.setContentText(item + " menu item selected");
+            alert.showAndWait();
+        }
+
+        private void doShowSaveDialog(String fileName) {
+            var alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Save Dialog");
+            alert.setHeaderText("Menu Item Saved Information");
+            alert.setContentText("'" + fileName + "' was successfully saved!");
             alert.showAndWait();
         }
     }
