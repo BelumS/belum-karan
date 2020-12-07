f4a09e4acab95fe1f40c406b4c39da612a51202c
Bel Sahn <beldsahn@gmail.com>
2020-12-07 15:41:55 -0600

    Completed TextEditor, and reverted task creation.

--- src/main/java/org/example/text/TextEditorController.java
+++ src/main/java/org/example/text/TextEditorController.java
@@ -14,8 +14,9 @@ public class TextEditorController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        String osName = System.getProperty("os.name");
         String javaVersion = System.getProperty("java.version");
         String javaFxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javaFxVersion + "\n Running on Java " + javaVersion + ".");
+        label.setText(String.format("Hello, JavaFX %s%n, running on %s%n with Java %s.%n",javaFxVersion, osName, javaVersion));
     }
 }
